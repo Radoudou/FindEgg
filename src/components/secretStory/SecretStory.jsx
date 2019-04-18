997be531658ece9f1370739517aaebc989d0eeeb
Isaure Blaffa <isaureblaffa.pro@gmail.com>
2019-04-18 22:32:43 +0200

    sauvegarde avant pull

---
+++ src/components/secretStory/SecretStory.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class SecretStory extends Component {
+    render(){
+        return (
+            <React.Fragment>
+                <img src='https://nsa40.casimages.com/img/2019/04/18/190418095705899893.png' alt='The Best Egg of the World'/>
+            </React.Fragment>
+        );
+    }
+}
