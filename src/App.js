9158b427ad7eca5db86757b7cd59c3b761102d34
walid <walid.hamiciwild@gmail.com>
2019-04-18 21:58:20 +0200

    add route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import Router from '../src/components/router/Router';
 import './App.css';
-import Header from '../src/components/Header/Header'
 class App extends Component {
   render() {
     return (
-        <Header/>
+      <React.Fragment>
+        <Router />
+      </React.Fragment>
     );
   }
 }
--- src/components/router/Router.jsx
+++ src/components/router/Router.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import HomePage from '../homePage/HomePage';
+import Easter from '../Easter/Easter';
 
 export default function Router() {
   return (
     <Switch>
       <Route path='/' component={HomePage} />
+      <Route path='/Easter' component={Easter}/>
     </Switch>
   );
 }
