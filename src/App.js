c93c8be90e3b6a61892f49a99a3568d8518dcef8
walid <walid.hamiciwild@gmail.com>
2019-04-18 19:14:17 +0200

    header Structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import Router from '../src/components/router/Router';
 import './App.css';
+import Header from '../src/components/Header/Header'
 class App extends Component {
   render() {
     return (
-      <React.Fragment>
-        <Router />
-      </React.Fragment>
+        <Header/>
     );
   }
 }
