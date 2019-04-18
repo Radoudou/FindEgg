dffb5f32eed8936c05d9c6078e2a83781ca3656e
Radoudou <Gtonepiece13@gmail.com>
2019-04-18 22:24:37 +0200

    add more route

---
+++ src/components/secretStory/SecretStory.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class SecretStory extends Component {
+    render() {
+        return (
+            <div>
+                <React.Fragment>
+                <div class="title ">
+                    <h1>Find the Eggious</h1>
+                    <img onClick="" alt="egg" className="element home-egg" src="https://trello-attachments.s3.amazonaws.com/5cb847b18973ce46369e81cd/626x626/c5bde99b68ccaf28a66a6bb53a24d52c/EggHoimepage.png" />
+                </div>
+                </React.Fragment>
+            </div>
+        )
+    }
+}
