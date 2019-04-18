bd965a25003ac1e81ec9ada7195763bb904cba13
walid <walid.hamiciwild@gmail.com>
2019-04-18 22:09:51 +0200

    add route

--- src/components/router/Router.jsx
+++ src/components/router/Router.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import HomePage from '../HomePage/HomePage';
+import HomePage from '../HomagePage/HomePage';
 import Easter from '../Easter/Easter';
 
 export default function Router() {
   return (
     <Switch>
-      <Route path='/' component={HomePage} />
+      <Route exact path='/' component={HomePage} />
       <Route path='/Easter' component={Easter}/>
     </Switch>
   );
