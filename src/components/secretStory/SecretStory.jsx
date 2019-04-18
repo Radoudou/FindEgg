fdad1729cfd449706ac177b9355f8dcd426bed2d
Radoudou <Gtonepiece13@gmail.com>
2019-04-18 22:28:51 +0200

    add folder header in folder Easter

--- src/components/secretStory/SecretStory.jsx
+++ src/components/secretStory/SecretStory.jsx
@@ -5,10 +5,7 @@ export default class SecretStory extends Component {
         return (
             <div>
                 <React.Fragment>
-                <div class="title ">
-                    <h1>Find the Eggious</h1>
-                    <img onClick="" alt="egg" className="element home-egg" src="https://trello-attachments.s3.amazonaws.com/5cb847b18973ce46369e81cd/626x626/c5bde99b68ccaf28a66a6bb53a24d52c/EggHoimepage.png" />
-                </div>
+                    
                 </React.Fragment>
             </div>
         )
