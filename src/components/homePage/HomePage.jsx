f043ec41877be4c2935b336897cfb88a884d9358
Radoudou <Gtonepiece13@gmail.com>
2019-04-18 19:35:49 +0200

    final homePage

--- src/components/homePage/HomePage.jsx
+++ src/components/homePage/HomePage.jsx
@@ -1,10 +1,50 @@
 import React from 'react';
-import './HomePage.css';
+import './HomePage.scss';
 
 export default function HomePage() {
     return (
         <React.Fragment>
-            <h1>Contenu à Ajouter</h1>
+            <div id="attribution"> <a href="https://is.gd/hQLAFp">Walid, Jean, Isaure, Radouane</a></div>
+            <div class="sky">
+                <div class="clouds">
+                    <div class="cloud30"></div>
+                    <div class="cloud29"></div>
+                    <div class="cloud28"></div>
+                    <div class="cloud27"></div>
+                    <div class="cloud26"></div>
+                    <div class="cloud25"></div>
+                    <div class="cloud24"></div>
+                    <div class="cloud23"></div>
+                    <div class="cloud22"></div>
+                    <div class="cloud21"></div>
+                    <div class="cloud20"></div>
+                    <div class="cloud19"></div>
+                    <div class="cloud18"></div>
+                    <div class="cloud17"></div>
+                    <div class="cloud16"></div>
+                    <div class="cloud15"></div>
+                    <div class="cloud14"></div>
+                    <div class="cloud13"></div>
+                    <div class="cloud12"></div>
+                    <div class="cloud11"></div>
+                    <div class="cloud10"></div>
+                    <div class="cloud9"></div>
+                    <div class="cloud8"></div>
+                    <div class="cloud7"></div>
+                    <div class="cloud6"></div>
+                    <div class="cloud5"></div>
+                    <div class="cloud4"></div>
+                    <div class="cloud3"></div>
+                    <div class="cloud2"></div>
+                    <div class="cloud1"></div>
+                    <div class="cloud0"></div>
+                </div>
+                <div class="title ">
+                    <h1>Find the Eggious</h1>
+                    <img onClick="" alt="egg" className="element home-egg" src="https://trello-attachments.s3.amazonaws.com/5cb847b18973ce46369e81cd/626x626/c5bde99b68ccaf28a66a6bb53a24d52c/EggHoimepage.png" />
+                </div>
+                <img id="run-rabbit" alt="rabbit" src="https://thumbs.gfycat.com/ExcellentCompassionateBernesemountaindog-max-1mb.gif" />
+            </div>
         </React.Fragment>
-  );
+    );
 }
