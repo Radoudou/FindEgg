5a2821b471362da12e704b786bf6b23eb01d7903
Isaure Blaffa <isaureblaffa.pro@gmail.com>
2019-04-19 03:12:01 +0200

    add Secret Story, à relire pour correction d'orthographe AVANT de merge

--- src/components/secretStory/SecretStory.jsx
+++ src/components/secretStory/SecretStory.jsx
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import EggStory from './EggStory'
+import './SecretStory.scss';
 
 export default class SecretStory extends Component {
     render(){
         return (
             <React.Fragment>
-                <img src='https://nsa40.casimages.com/img/2019/04/18/190418095705899893.png' alt='The Best Egg of the World'/>
+                <div className="skySecret">
+                    <div className="cloudSecret">
+                        <div className="cloud1Secret"></div>
+                        <img src='https://nsa40.casimages.com/img/2019/04/18/190418095705899893.png' alt='The Best Egg of the World'/>
+                        <div className="cloud0Secret"></div>
+                        <EggStory />
+                        <img src='https://nsa40.casimages.com/img/2019/04/18/190418095705899893.png' alt='The Best Egg of the World'/>
+                    </div>
+                </div>
             </React.Fragment>
         );
     }
